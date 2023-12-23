60861b2541097a23ff94507ae01e8b2832f6c74d
kanghanju <66769475+kanghanju@users.noreply.github.com>
2023-12-23 19:08:44 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231223/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    static int n,k;
+    static ArrayList<Integer> arr = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        k = sc.nextInt();
+        n = sc.nextInt();
+
+        choose(1);
+    }
+
+    public static void choose(int num){
+        if(num == n+1){
+            print();
+            return;
+        }
+
+        for(int i = 1; i <= k; i++){
+            if(num >= 2 && arr.get(i-2) == arr.get(i-1) && arr.get(i-1) == arr.get(i)){
+                continue;
+            }
+            arr.add(i);
+            choose(num+1);
+            arr.remove(arr.size()-1);
+        }
+    }
+
+    public static void  print(){
+        for(int i = 0; i < arr.size(); i++){
+            System.out.print(arr.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
