ed96a961096c4633ecb46f49eb2d6be12a367992
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-04-18 18:03:28 +0900

    Title: 그래프 탐색, Time: 212ms, Memory: 14MB, Status: Passed - CodeTree

--- 240418/그래프 탐색/graph-traversal.java
+++ 240418/그래프 탐색/graph-traversal.java
@@ -2,16 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    public static final int MAX_NUM = 1000;
+    public static int n, m;
+
+    public static List<Integer>[] graph = new ArrayList[MAX_NUM + 1];
+    public static boolean[] visited = new boolean[MAX_NUM + 1];
+    public static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        boolean[][] arr = new boolean[n + 1][n + 1];
-        boolean[] visited = new boolean[n + 1];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <=n; i++) {//이 부분 주의 
+            graph[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -19,24 +29,25 @@ public class Main {
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            arr[x][y] = true;
-            arr[y][x] = true;
+            graph[x].add(y);
+            graph[y].add(x);
         }
-        
+
         visited[1] = true;
-        int answer = dfs(arr,visited,1,n,0);
+        dfs(1);
 
         System.out.println(answer);
     }
 
-    private static int dfs(boolean[][] arr, boolean[] visited, int next,int n,int answer) {
-        for(int i = 1; i <= n; i++) {
-            if(arr[next][i] && !visited[i]) {//next노드와 이어져있지만 방문하지 않은 노드
-                visited[i] = true;
-                answer = dfs(arr,visited,i,n,answer + 1);
+    private static void dfs(int vertex) {
+        //주어진 정점에서 이어져있는 모든 정점을 탐색한다. graph[vertex].size()
+        for(int i = 0; i < graph[vertex].size(); i++) {
+            int currV = graph[vertex].get(i);
+            if(!visited[currV]) {
+                visited[currV] = true;
+                answer++;
+                dfs(currV);
             }
         }
-        
-        return answer;
     }
 }
